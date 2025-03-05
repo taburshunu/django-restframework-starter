a7e7fba3ac78df1ae200f954d48480807650b17e
taburshunu <tcholuminary@gmail.com>
2025-03-05 13:45:04 +0500

    fixed some errors

--- a_users/models.py
+++ a_users/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='avatars/', null=True, blank=True, default='static/images/ape1.jpg')
+    image = models.ImageField(upload_to='avatars/', null=True, blank=True)
     displayname = models.CharField(max_length=20, null=True, blank=True)
     info = models.TextField(null=True, blank=True) 
     
